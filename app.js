8a548953913d2cb75e5958f44a54c32069809f5c
Will Cook <nzwillcook@gmail.com>
2016-10-18 16:16:28 +0100

    added missing colons

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var jsonServer = require('json-server')
-var clone = require('clone')
-var data = require('./doughnuts.json')
+var jsonServer = require('json-server');
+var clone = require('clone');
+var data = require('./doughnuts.json');
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
-var app = jsonServer.create()
-var router = jsonServer.router(clone(data))
+var app = jsonServer.create();
+var router = jsonServer.router(clone(data));
 
 app.all('*', function(req, res, next) {
-    router.db.object = clone(data)
-    next()
-})
+    router.db.object = clone(data);
+    next();
+});
 
-app.use(jsonServer.defaults)
-app.use(router)
+app.use(jsonServer.defaults);
+app.use(router);
 
 app.listen(port, function() {
-    console.log('Doughnuts listening on http://localhost:' + port)
-})
+    console.log('Doughnuts listening on http://localhost:' + port);
+});
